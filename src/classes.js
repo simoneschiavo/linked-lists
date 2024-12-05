928f84185a393f4de605ec319fe836f63e182e88
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-05 15:08:23 +0100

    First commit

---
+++ src/classes.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(value, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+};
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null && this.tail === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.nextNode = newNode;
+            this.tail = newNode;
+        }
+    }
+}
