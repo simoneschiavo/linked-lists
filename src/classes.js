b4280298f9c2bf37bf083b49dbc842098a36983b
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-05 15:35:00 +0100

    Add the size method to LinkedList

--- src/classes.js
+++ src/classes.js
@@ -1,36 +1,48 @@
 class Node {
-    constructor(value, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-    }
-};
+  constructor(value, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-    append(value) {
-        const newNode = new Node(value);
-        if (this.head === null && this.tail === null) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.nextNode = newNode;
-            this.tail = newNode;
-        }
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null && this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
     }
+  }
 
-    prepend(value) {
-        const newNode = new Node(value);
-        if (this.head === null && this.tail === null) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            const oldHead = this.head;
-            this.head = newNode;
-            this.head.nextNode = oldHead;
-        }
+  prepend(value) {
+    const newNode = new Node(value);
+    if (this.head === null && this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const oldHead = this.head;
+      this.head = newNode;
+      this.head.nextNode = oldHead;
     }
+  }
+
+  size() {
+    let numberOfNodes = 0;
+    if (this.head === null) {
+      return numberOfNodes;
+    } else {
+      for (let currentNode = this.head; currentNode !== null; numberOfNodes++) {
+        currentNode = currentNode.nextNode;
+      }
+      return numberOfNodes;
+    }
+  }
 }
